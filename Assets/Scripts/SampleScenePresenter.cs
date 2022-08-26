c2d4bddcfc3e4193201785d1eb740d9f2a7123d2
sena_kuzuoka <senakuzuoka@gmail.com>
2022-08-27 05:53:24 +0900

    StreamingAssets以下の画像を表示

--- Assets/Scripts/SampleScenePresenter.cs
+++ Assets/Scripts/SampleScenePresenter.cs
@@ -2,14 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+using UnityEngine.Networking;
 
 public class SampleScenePresenter : MonoBehaviour
 {
-    [SerializeField] Text text;
+    [SerializeField] Renderer renderer;
 
     void Start()
     {
-        Debug.Log($"{HelloWorld.FooPluginFunction()}");
-        text.text = $"{HelloWorld.FooPluginFunction()}";
+        StartCoroutine(ShowPicture());
+    }
+
+    IEnumerator ShowPicture()
+    {
+        var path = "file://" + Path.Combine(Application.streamingAssetsPath, "cat.png");
+        Debug.Log(path);
+
+        var request = UnityWebRequestTexture.GetTexture(path);
+        yield return request.SendWebRequest();
+
+        if (request.result == UnityWebRequest.Result.ConnectionError)
+        {
+            Debug.Log(request.error);
+        }
+        else {
+            var sourceTexture = ((DownloadHandlerTexture)request.downloadHandler).texture;
+            renderer.material.mainTexture = sourceTexture;
+        }
+
+        request.Dispose();
+
+        yield return null;
     }
 }
